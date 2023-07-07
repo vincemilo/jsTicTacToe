edc9bddd3828f8ae92074746e6d3ce1fea1b09a5
vincemilo <vincemilo@gmail.com>
2023-07-07 12:55:29 -0600

    playing around with factory functions

--- app.js
+++ app.js
@@ -1,20 +1,55 @@
 const GameBoard = (() => {
-    const board = [
-        [0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0]
-        ];
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+    for (let i = 0; i < rows; i++){
+        board[i] = [];
+        for (let j = 0; j < columns; j++){
+            board[i].push(Cell());
+        }
+    }
     const getBoard = () => board;
-    return {getBoard};
+    const dropToken = (row, column, player) => {
+        board[row][column] = player;
+    }
+    const printBoard = () => {
+        const boardWithCellValues = board.map((row) => row.map((cell) => cell))//.map((cell) => cell.getValue()));
+        console.log(boardWithCellValues);
+    }
+    return {getBoard, dropToken, printBoard};
 })();
 
-console.log(GameBoard.getBoard());
+function Cell() {
+    let value = 0;
+    const addToken = (player) => {
+        value = player;
+    }
+
+    const getValue = () => value;
+
+    return {addToken, getValue};
+};
+
+GameBoard.dropToken(0, 1, 1);
+GameBoard.printBoard();
 
 const Player = (name) => {
     const getName = () => console.log(name);
     return {getName};
 }
 
-const player1 = Player('vince');
+// const player1 = Player('vince');
 
-player1.getName();
+// player1.getName();
+
+// const gameStart = (() => {
+//     console.log('Welcome to tic tac toe');
+//     const player1 = Player('vince');
+//     const player2 = Player('comp');
+//     return {player1, player2};
+// })();
+
+// console.log(gameStart);
+const selectSquare = player => {
+   
+}
